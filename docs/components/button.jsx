7f0db5223c4ca5eb9dba5fe03c0dc7f124bc827f
Maxine <43437731+xxiangaa@users.noreply.github.com>
2022-03-15 15:11:28 +0800

    fix: pdf rendering

--- docs/components/button.jsx
+++ docs/components/button.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
-import Button from '@mui/material/Button';
+import React from "react";
+import Button from "@mui/material/Button";
+import Pagination from "@mui/material/Pagination";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
+import { Stack } from "@mui/material";
+
+const url = "https://d6i2izex24s5q.cloudfront.net/etd-whitepaper-chinese.pdf";
 
 export default function button() {
+  const [numPages, setNumPages] = React.useState(null);
+  const [pageNumber, setPageNumber] = React.useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
   return (
-    <Button variant="outlined"><a href='https://d6i2izex24s5q.cloudfront.net/etd-whitepaper-chinese.pdf'>ETD WhitePaper</a></Button>
-  )
+    <Stack
+      style={{ width: "100%" }}
+      alignContent={"center"}
+      justifyContent="center"
+    >
+      <Pagination
+        count={numPages}
+        style={{ width: "100%" }}
+        onChange={(e, page) => {
+          setPageNumber(page);
+        }}
+      />
+      <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <Button
+        variant="outlined"
+        onClick={() => {
+          window.open(url, "_blank").focus();
+        }}
+      >
+        Download ETD WhitePaper
+      </Button>
+    </Stack>
+  );
 }
