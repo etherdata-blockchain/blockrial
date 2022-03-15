7336f63b7095ef7c58bc21d531d10ba16d7266df
Maxine <43437731+xxiangaa@users.noreply.github.com>
2022-03-15 12:49:14 +0800

    feat: :art: add etd

---
+++ docs/components/button.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+export default function button() {
+  return (
+    <Button variant="outlined"><a href='https://d6i2izex24s5q.cloudfront.net/etd-whitepaper-chinese.pdf'>ETD WhitePaper</a></Button>
+  )
+}
